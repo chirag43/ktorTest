39eaa889f11dfebbf26b855676c5c768d7dbd108
chirag <chirag.patel.code@gmail.com>
2023-04-19 23:46:04 -0400

    Use kotlin version 1.8.10 and also upgrade gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
 }
@@ -32,10 +32,11 @@ dependencies {
     implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation(kotlin("stdlib-jdk8"))
+
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation(kotlin("stdlib-jdk8"))
 }
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
